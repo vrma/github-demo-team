f6e06fc185d468ba20f71287ba315dc992e62add
vrmachado <vrmachado@gmail.com>
2025-03-19 11:05:54 +0100

    objetos estudiantes renderizados

--- JS/formulario.js
+++ JS/formulario.js
@@ -4,16 +4,59 @@ const formulario = document.querySelector("#formulario1");
 
 formulario.addEventListener("submit", (event) => {
     event.preventDefault();
-    alert("Se ha impedido el envio normal del formulario");
+    // alert("Se ha impedido el envio normal del formulario");
 
     const objEstudiante = new Object();
 
     objEstudiante.nombre = document.querySelector("#nombre").value;
     objEstudiante.primerApellido = document.querySelector("#primerApellido").value;
+    objEstudiante.segundoApellido = document.querySelector("#segundoApellido").value;
+    objEstudiante.genero = document.querySelector("input[name=genero]:checked").value;
+    objEstudiante.fechaAlta = document.querySelector("#fechaAlta").value;
+    objEstudiante.facultad = document.querySelector("#facultad").value;
 
     arrayDeObjetosEstudiante.push(objEstudiante);
 
     console.log("El array de Objetos Estudiantes: ");
     console.log(arrayDeObjetosEstudiante);
+
+    // Agregar el estudiante al cuerpo de la tabla
+
+    const tableBody = document.querySelector("#tableBody");
+
+    // Eliminar previamente los hijos del elemento cuyo id es tableBody
+    while (tableBody.firstChild) {
+        tableBody.removeChild(tableBody.firstChild);
+    }
+
+    // Recorrer el array de objeto estudiante
+    for (const objEstudiante of arrayDeObjetosEstudiante) {
+        
+        const fila = document.createElement("tr");
+
+        const nombre = document.createElement("td");
+        const primerApellido = document.createElement("td");
+        const segundoApellido = document.createElement("td");
+        const genero = document.createElement("td");
+        const fechaAlta = document.createElement("td");
+        const facultad = document.createElement("td");
+
+        nombre.textContent = objEstudiante.nombre;
+        primerApellido.textContent = objEstudiante.primerApellido;
+        segundoApellido.textContent = objEstudiante.segundoApellido;
+        genero.textContent = objEstudiante.genero;
+        fechaAlta.textContent = objEstudiante.fechaAlta;
+        facultad.textContent = objEstudiante.facultad;
+
+        fila.appendChild(nombre);
+        fila.appendChild(primerApellido);
+        fila.appendChild(segundoApellido);
+        fila.appendChild(genero);
+        fila.appendChild(fechaAlta);
+        fila.appendChild(facultad);
+
+        tableBody.appendChild(fila);
+    }
+
 
 });
